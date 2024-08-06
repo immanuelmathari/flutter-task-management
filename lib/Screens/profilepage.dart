3891fee0b396d10aa720c81f455072cdf3976f6f
immanuelmathari <immanuelnganga2001@gmail.com>
2024-08-06 20:19:32 +0300

    start of profile page

--- lib/Screens/profilepage.dart
+++ lib/Screens/profilepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskmanagement/widgets/constants.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -11,6 +12,136 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     var he = MediaQuery.of(context).size.height;
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: MyThemeColor.backgroundColor,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.only(left: he * 0.03, right: he * 0.03, top: he * 0.03, bottom: he * 0.03),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // everythin we need
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Text('First Name', style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              )),
+              SizedBox(
+                height: he * 0.01,
+              ),
+              SizedBox(
+                height: he * 0.045,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.only(left: 10, top: 12,),
+                    hintText: 'Immanuel',
+                    hintStyle: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        color: MyThemeColor.textColor
+                      )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: MyThemeColor.textColor),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: MyThemeColor.textColor),
+                    )
+                  )
+                )
+              ),
+              SizedBox(
+                height: he * 0.02,
+              ),
+              Text('Last Name', style: TextStyle(
+                color: Colors.grey[600],
+                fontWeight: FontWeight.bold,
+              )),
+              SizedBox(
+                height: he * 0.01,
+              ),
+              SizedBox(
+                height: he * 0.45,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.only(left: 10, top: 12),
+                    hintText: "Mathari",
+                    hintStyle: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: MyThemeColor.textColor)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: MyThemeColor.textColor),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: MyThemeColor.textColor)
+                    ),
+                  )
+                )
+              ),
+              SizedBox(
+                height: he * 0.02,
+              ),
+              Text(
+                'Username',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    height: he * 0.045,
+                    width: he * 0.25,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.only(left: 10, top: 12),
+                        hintText: 'Mathari Immanue',
+                        hintStyle: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: MyThemeColor.textColor),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: MyThemeColor.textColor),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: MyThemeColor.textColor),
+                        )
+                      )
+                    )
+                  )
+                ]
+              )
+            ]
+          )
+        )
+      )
+    );
   }
 }
